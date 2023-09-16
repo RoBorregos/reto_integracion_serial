810f0f849f951d4ca0fa45d78c9680aea5ef7679
IvanRomero03 <i.wells.ar@gmail.com>
2023-09-15 20:56:16 -0600

    Init Reto

---
+++ src/motor/scripts/motor_node.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import sourcedefender
+from os import environ
+
+environ["SOURCEDEFENDER_PASSWORD"] = "RoBorregos2023"
+environ["SOURCEDEFENDER_SALT"] = "RoBorregos#1"
+import motor
+
+#motor = Motor()
+#motor.run()
